ac0d6fd61883268858b46f4d897c1553d8933393
Irina Bugrova <irina92-08@mail.ru>
2025-11-23 21:40:06 +0300

    fix errors lint

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,16 +1,15 @@
-// eslint.config.js
-import js from "@eslint/js";
-import globals from "globals";
-import reactPlugin from "eslint-plugin-react";
-import reactHooks from "eslint-plugin-react-hooks";
-import functionalPlugin from "eslint-plugin-functional";
+import js from '@eslint/js';
+import globals from 'globals';
+import reactPlugin from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
 
 export default [
+  // Базовые настройки
   {
-    files: ["**/*.js", "**/*.jsx"],
+    files: ['**/*.js', '**/*.jsx'],
     languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
+      ecmaVersion: 'latest',
+      sourceType: 'module',
       parserOptions: {
         ecmaFeatures: {
           jsx: true,
@@ -22,88 +21,52 @@ export default [
         ...globals.node,
       },
     },
-  },
-  js.configs.recommended,
-  {
-    files: ["**/*.jsx", "**/*.js"],
     plugins: {
       react: reactPlugin,
+      'react-hooks': reactHooks,
     },
     rules: {
+      // Базовые правила
+      ...js.configs.recommended.rules,
       ...reactPlugin.configs.recommended.rules,
-      "react/react-in-jsx-scope": "off",
-      "react/prop-types": "off",
-      "react/function-component-definition": [
-        "error",
-        {
-          namedComponents: "arrow-function",
-        },
+      ...reactHooks.configs.recommended.rules,
+      
+      // Кастомные правила
+      'react/prop-types': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'import/extensions': 'off',
+      'import/no-unresolved': 'off',
+      'no-console': 'off',
+      'no-underscore-dangle': [
+        'error', 
+        { 
+          allow: ['__filename', '__dirname'] 
+        }
       ],
-      "react/jsx-filename-extension": [
-        "warn",
-        {
-          extensions: [".js", ".jsx"],
-        },
+      'react/function-component-definition': [
+        'error', 
+        { 
+          namedComponents: 'arrow-function' 
+        }
+      ],
+      'react/jsx-filename-extension': [
+        'warn', 
+        { 
+          extensions: ['.js', '.jsx'] 
+        }
       ],
     },
     settings: {
       react: {
-        version: "detect",
+        version: 'detect',
       },
     },
   },
   {
-    files: ["**/*.jsx", "**/*.js"],
-    plugins: {
-      "react-hooks": reactHooks,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-    },
-  },
-  {
-    files: ["**/*.js", "**/*.jsx"],
-    plugins: {
-      functional: functionalPlugin,
-    },
-    rules: {
-      "functional/functional-parameters": "off",
-      "functional/immutable-data": "off",
-      "functional/no-conditional-statements": "off",
-      "functional/no-expression-statements": "off",
-      "functional/no-promise-reject": "error",
-      "functional/no-return-void": "off",
-      "functional/no-throw-statements": "off",
-      "functional/no-try-statements": "off",
-      "functional/no-mixed-types": "off",
-      "functional/prefer-immutable-types": "off",
-      "functional/prefer-readonly-type": "off",
-      "functional/prefer-tacit": "off",
-      "functional/no-class": "off",
-    },
-  },
-  {
-    files: ["**/*.js", "**/*.jsx"],
-    rules: {
-      "import/extensions": "off",
-      "import/no-unresolved": "off",
-      "no-console": "off",
-      "no-underscore-dangle": [
-        "error",
-        {
-          allow: ["__filename", "__dirname"],
-        },
-      ],
-    },
-  },
-  {
     ignores: [
-      "dist/**",
-      "build/**",
-      "node_modules/**",
-      "coverage/**",
-      "*.min.js",
-      "*.bundle.js",
+      'node_modules/**',
+      'dist/**',
+      'build/**',
     ],
   },
 ];
