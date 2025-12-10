db7c5fa602b2440ccbab36abde418b4c4b628c50
Irina Bugrova <irina92-08@mail.ru>
2025-12-10 18:43:36 +0300

    add rollbar

--- frontend/rollbar-config.js
+++ frontend/rollbar-config.js
@@ -5,7 +5,7 @@ const rollbarConfig = {
     "c0026754848148b3b0664af4de8b6edda408e2487d74b64dcc2707bd6724de0b96d0dd086ae83782469c9ee8d77195b3",
   captureUncaught: true,
   captureUnhandledRejections: true,
-  environment: "development",
+  environment: "production",
   payload: {
     client: {
       javascript: {
